991e3c1e5fc37c59c6d1533da4fccb462c4e0dee
folakeatolagbe96 <folakeatolsgbe96@gmail.com>
2023-02-18 16:43:03 -0800

    Task 11

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - prints all combinations of singletion of single digits
+ * main - main block
+ * Descripution: prints all single number of base 10
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
+	int c = 0;
 
-	for (n - 48; n < 58; n++)
+	while (c < 10)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(48 + c);
+		if (c != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
