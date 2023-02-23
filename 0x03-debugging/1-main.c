400943cc6701d5551981e7cea0d8d827b8f46e2f
folakeatolagbe96 <folakeatolsgbe96@gmail.com>
2023-02-22 17:57:49 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,14 @@ int main(void)
 	printf("infinte loop incoming :(\n");
 
 	i = 0;
+
 	/*
-	 *while (i < 10)
-	 *{
-	 *              putchar(i);
+	 *while (i < 10)*/
+	 *{*/
+	 *              putchar(i);*/
 	 *}
-	 */
+	 */*/
 	printf("infinte loop avoided! \\o/\n");
 
 	return (0);
-
 }
