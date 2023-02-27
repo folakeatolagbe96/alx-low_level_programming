1a9d9e233ed5dd4f70c912f5aa706089703b15b7
folakeatolagbe96 <folakeatolsgbe96@gmail.com>
2023-02-27 02:07:27 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *_puts - prints a strin
+ *_puts - prints a string
  *@str: string to be printed
+ *Return: void
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str**);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
