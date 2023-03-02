39cb5bfbee16c3ab54f2d9ffe57283a4eb804dc1
folakeatolagbe96 <folakeatolsgbe96@gmail.com>
2023-03-01 16:02:00 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcat - Concatenates two strings
- * @dest: The destination string
- * @src: The source string
- * Return: A ponter to the resulting string dest
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
