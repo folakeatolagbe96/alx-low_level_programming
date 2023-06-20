b29f1e9386fbf83a0a566e37ae338c5a2d935ad2
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-19 18:16:27 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - p print all possible different combinations of these digits
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int d, p, q;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (d  = '0'; d < '9'; d++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
-		{
-			for (ones = '0'; ones <= '9'; ones++)
-			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
-				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+	for (p = d + 1; p <= '9'; p++)
+	{
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '9' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
