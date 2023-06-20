c34e9b445d7207438cc0c0c9de76ceb23bb18891
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-20 05:59:02 -0700

    betty 4-isalpha.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The characterto be checked
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int _lower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
