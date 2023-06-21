c45483210ff1bec1dbea3b2893d7b3a84942d8bf
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-20 19:09:56 -0700

    ./11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 
 #include <stdio.h>
 
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			{
 			printf("%d", n);
 			printf("\n");
-			break
+			break;
 		}
 		else
 		{
@@ -34,11 +34,11 @@ void print_to_98(int n)
 			{
 				printf("%d", n);
 				printf("\n");
-				break
+				break;
 			}
 			else
 			{
-				printff("%d, ", n);
+				printf("%d, ", n);
 			}
 		}
 	}
