c914d458afc2b8ec5efb5ef62a14bc19d8569b1b
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-26 05:05:14 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes apointer to an int a parameter
+ *  and updates the value it pints to 98
+ *  @n: input
+ *  Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
