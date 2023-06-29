a8102f66a49543854500b92c61d039f4f895e367
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-29 10:16:04 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,14 +6,13 @@
  * @s: pointer to string params
  * Return: *s
  */
-
 char *rot13(char *s)
 {
 	int i;
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
