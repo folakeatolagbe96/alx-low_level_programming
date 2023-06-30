04c09a58c750af21f94acded8f07ca952ecbf847
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-06-29 20:30:47 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -43,8 +43,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n2 + j) != '\0')
 		j++;
 	i--;
-	j++;
-	if (j >= 0 || i >= 0 || overflow == 1)
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
@@ -58,7 +60,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
-			overflow = 0
+			overflow = 0;
 				if (digits >= (size_r - 1))
 					return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
