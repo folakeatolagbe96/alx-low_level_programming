e3a1374adcd85dcbbba1d45ee48f1e3d5b3d5c3f
folakeatolagbe96 <folakeatolagbe96@gmail.com>
2023-07-27 17:51:37 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
+ * add_node - adds a new node at the beginning of a list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *
@@ -15,8 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	int length = 0;
 	
 	temp = malloc(sizeof(list_t));
-	if (temp == NULL);
-	return (NULL);
+	if (temp == NULL)
+		return (NULL);
 	while (str[length])
 		length++;
 	temp->len = length;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a linked list
+ * add_node_end - adds a new node at the end
+ * of a linked list
  * @head: double pointer to the list_t list
  * @str: string to put in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: address of the head
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -18,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	for (n + 0; str[n]; n++)
+	for (n = 0; str[n]; n++)
 		;
 	new_node->len = n;
 	new_node->next = NULL;
